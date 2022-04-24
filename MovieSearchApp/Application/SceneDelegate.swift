babc7e2049c6c214b28ec7cf4c7380d53abec2db
“DalioKim” <ejxlabvlzhxm@naver.com>
2022-04-24 18:42:21 +0900

    FEAT: 메인 검색화면 구현 1. Application, Domain등 기본 디렉토리 구조 구성 2. SnapKit, RxSwift등 기본 라이브러리 설치 2. MainSearch VC에 searchBar,collectionView등 기본 UI 로직 구현

---
+++ MovieSearchApp/Application/SceneDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  SceneDelegate.swift
+//  Assignment
+//
+//  Created by 김동현 on 2022/04/24.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    
+    var window: UIWindow?
+    
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        window = UIWindow(windowScene: windowScene)
+        window?.backgroundColor = .white
+        
+        let navigationController = UINavigationController(rootViewController: MainSearchViewController())
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
+    }
+}
---
+++ MovieSearchApp/Domain/MainSearchViewController.swift
@@ -0,0 +1,95 @@
+//
+//  MainSearchViewController.swift
+//  Assignment
+//
+//  Created by 김동현 on 2022/04/24.
+//
+
+import UIKit
+import SnapKit
+import RxSwift
+import RxCocoa
+
+class MainSearchViewController: UIViewController {
+    private lazy var stackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [searchBar, collectionView])
+        stackView.axis = .vertical
+        stackView.spacing = 10
+        stackView.alignment = .center
+        
+        searchBar.snp.makeConstraints {
+            $0.top.equalToSuperview()
+            $0.height.equalTo(40)
+            $0.leading.trailing.equalToSuperview()
+        }
+        collectionView.snp.makeConstraints {
+            $0.top.equalTo(searchBar.snp.bottom).offset(10)
+            $0.bottom.equalToSuperview()
+            $0.leading.trailing.equalToSuperview()
+        }
+        
+        return stackView
+    }()
+    
+    private let searchBar = UISearchBar()
+    
+    private let collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        layout.sectionInset = UIEdgeInsets(top: 10, left: 20, bottom: 10, right: 20)
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        return collectionView
+    }()
+    
+    private let disposeBag = DisposeBag()
+    
+    // MARK: - Init
+    
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupViews()
+        bindTitle()
+        bindCollectionView()
+    }
+    
+    // MARK: - private
+    
+    private func setupViews() {
+        view.addSubview(stackView)
+        
+        stackView.snp.makeConstraints {
+            $0.edges.equalTo(view.safeAreaLayoutGuide)
+        }
+        
+    }
+    
+    private func bindTitle() {
+        rx.methodInvoked(#selector(viewWillAppear(_:)))
+            .subscribe(onNext: { [weak self] _ in
+                self?.title = "네이버 영화 검색"
+            })
+            .disposed(by: disposeBag)
+    }
+    
+    private func bindCollectionView() {
+        collectionView.rx.setDelegate(self).disposed(by: disposeBag)
+    }
+}
+
+// MARK: - UICollectionViewDelegateFlowLayout
+
+extension MainSearchViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = collectionView.frame.size.width - 20
+        return CGSize(width: width, height: 200)
+    }
+}
